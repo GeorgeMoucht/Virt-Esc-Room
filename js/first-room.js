92e0abd391ad0ced1495c363c2259ed5fb67dfe0
spyrosFL <spyrosfelekidis@gmail.com>
2021-07-15 15:17:22 +0300

    Init template

---
+++ js/first-room.js
@@ -0,0 +1,19 @@
+
+/*
+var l8 = document.getElementById("drag1");
+var parl1 = node.parl1;
+console.log(parl1);
+
+*/
+
+var score2Element = document.getElementById("petalouda");
+
+var score2ElementT = document.getElementById("drag1");
+
+
+if( score2Element == score2ElementT) {
+    console.log("true")
+}else {
+    console.log("false")
+
+}
